25bcb4b8bdb2fbc9ea5aed041dacf704721dd7df
Akram F <kharmannn@gmail.com>
2023-03-18 08:41:43 +0700

    added new xml test able to run selected test cases

---
+++ src/test/java/mycompany/qa/Android/AppiumTest.java
@@ -0,0 +1,13 @@
+package mycompany.qa.Android;
+
+import org.apache.log4j.BasicConfigurator;
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+    @Test
+    public void Android() {
+        System.out.println("executing android test");
+    }
+
+}
---
+++ src/test/java/mycompany/qa/Android/AppiumTest2.java
@@ -0,0 +1,12 @@
+package mycompany.qa.Android;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest2 {
+
+    @Test
+    public void Android() {
+        System.out.println("just another test");
+    }
+
+}
